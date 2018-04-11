b53e351f6fbb031cd29ef3a835a935e2a953cfca
ipukhov <ivan.pukhov@cogniteq.com>
2018-04-11 16:21:32 +0300

    some improvements

--- src/main/java/com/example/demo/oauth2/client/Oauth2ClientApplication.java
+++ src/main/java/com/example/demo/oauth2/client/Oauth2ClientApplication.java
@@ -1,12 +1,28 @@
 package com.example.demo.oauth2.client;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.security.Principal;
+
+@EnableAutoConfiguration
+@Configuration
+@EnableOAuth2Sso
+@RestController
 public class Oauth2ClientApplication {
 
+	@RequestMapping("/")
+	public String home(Principal user) {
+		return "Hello " + user.getName();
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(Oauth2ClientApplication.class, args);
+		new SpringApplicationBuilder(Oauth2ClientApplication.class)
+				.properties("spring.config.name=client").run(args);
 	}
 }
